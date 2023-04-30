4d062f850e5bc0513e21f4ee0935f54964947164
SuShruthi <shruthacm@gmail.com>
2023-04-30 12:04:00 -0700

    add delete function to the index page

--- express_server.js
+++ express_server.js
@@ -49,6 +49,12 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
+app.post("/urls/:id/delete", (req, res) => {
+  const shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
