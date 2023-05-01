62af08666a280ba17d419a5f9564b5ec07167150
SuShruthi <shruthacm@gmail.com>
2023-04-30 21:27:18 -0700

    Logout and Clear Cookies

--- express_server.js
+++ express_server.js
@@ -67,6 +67,10 @@ app.post("/login", (req, res) => {
   res.cookie("username", username);
   res.redirect("/urls");
 });
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
 
 app.listen(PORT, () => {
