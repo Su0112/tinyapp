6bb953dfecec3d57c8613429085e0c890fdf6925
SuShruthi <shruthacm@gmail.com>
2023-04-30 19:10:08 -0700

    add form, edit and post routes to urls_index and shows page

--- express_server.js
+++ express_server.js
@@ -54,6 +54,13 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
+app.post("/urls/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
+
 
 
 app.listen(PORT, () => {
